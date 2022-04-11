3c829c91def8f569f5ea2ca81eaa7f9207cac50a
Axel Hernández <89049642+AxRex1473@users.noreply.github.com>
2022-04-11 15:04:38 -0500

    sistema de puntos
    Aun  no he logrado dar con el sistema de puntos

--- Assets/Script/SistemaPuntos.cs
+++ Assets/Script/SistemaPuntos.cs
@@ -6,15 +6,5 @@ using TMPro;
 
 public class SistemaPuntos : MonoBehaviour
 {
-    public TextMesh textoContador;
-    private int puntuacion = 0;
-    void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log (other.gameObject.tag);
-        if (other.gameObject.tag == "collectable")
-        {
-            puntuacion = puntuacion + 1;
-            textoContador.text = puntuacion.ToString();
-        }
-    }
+    
 }
