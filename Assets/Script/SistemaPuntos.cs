e2252a85507e15fec4f607fff58c6844e0af6bca
Axel Hernández <89049642+AxRex1473@users.noreply.github.com>
2022-04-11 15:23:30 -0500

    Cambios Codigo

--- Assets/Script/SistemaPuntos.cs
+++ Assets/Script/SistemaPuntos.cs
@@ -6,5 +6,21 @@ using TMPro;
 
 public class SistemaPuntos : MonoBehaviour
 {
-    
+    [SerializeField]
+    public TextMeshProUGUI Text;
+    public int score = 0;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Puntos")
+        {
+            score = score + 1;
+            Text.text = Text.ToString();
+        }
+    }
 }
