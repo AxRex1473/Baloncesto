fd406744a9ca9deeb24c8ca45128c09b89155e1d
Leonardo-cDev <63112538+Leonardo-cDev@users.noreply.github.com>
2022-04-14 15:14:51 -0500

    animacion de manos

--- Assets/_Script/SistemaPuntos.cs
+++ Assets/_Script/SistemaPuntos.cs
@@ -14,6 +14,7 @@ public class SistemaPuntos : MonoBehaviour
     public SpawnConfetti _spawnConfetti;
     public bool _encesto = default;
     private int _currentScore;
+    public Animator _anim;
 
     void Start()
     {
@@ -23,7 +24,7 @@ public class SistemaPuntos : MonoBehaviour
     private void HandleScore()
     {
         _scoreText.text = "Score: " + _currentScore.ToString();
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,20 +32,21 @@ public class SistemaPuntos : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Tocando");
-            _currentScore ++;
+            _currentScore++;
             HandleScore();
             _encesto = true;
         }
 
-        if (_encesto == true )
+        if (_encesto == true)
         {
             _spawnConfetti.Confeti();
             _canastaRandom.transform.position = new Vector3(Random.Range(-45f, -15f), 7.4f, Random.Range(45f, -45f));
             _enemigo.transform.position = new Vector3(_canasta.transform.position.x + Random.Range(10, 15), _canasta.transform.position.y - 8, _canasta.transform.position.z + Random.Range(-10, 10));
             _ball.transform.position = new Vector3(0, 3f, 0);
+            StartCoroutine(Manos());
         }
 
-        if(other.gameObject.CompareTag("Piso"))
+        if (other.gameObject.CompareTag("Piso"))
         {
             _ball.transform.position = new Vector3(0, 3f, 0);
         }
@@ -53,5 +55,12 @@ public class SistemaPuntos : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) _encesto = false;
+    }
+
+    IEnumerator Manos()
+    {
+        _anim.SetBool("ManosAnim", true);
+        yield return new WaitForSeconds(3);
+        _anim.SetBool("ManosAnim", false);
     }
 }
